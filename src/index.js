6d03cf42d157de150dc104c6b4cbba29350c84a6
Meagan Blais <meaganblais@gmail.com>
2018-04-24 20:26:56 -0400

    setting up routes

--- src/index.js
+++ src/index.js
@@ -2,14 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-import App from './components/app';
 import reducers from './reducers';
+import PostsIndex from './components/posts_index';
+import PostsShow from './components/posts_show';
+import PostsNew from './components/posts_new';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={PostsIndex} />
+        <Route path="/posts/new" component={PostsNew} />
+        <Route path="/posts/:id" component={PostsShow} />
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
