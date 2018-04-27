8e538a87fbf2161682cb0ca2a2c760960992f6b7
Meagan Blais <meaganblais@gmail.com>
2018-04-27 10:40:02 -0400

    setting up redux forms

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
 
 class PostsNew extends Component {
+  renderTitleField(field) {
+    return (
+      <div>
+        <input
+          type="text"
+          {...field.input}
+        />
+      </div>
+    );
+  }
+
   render() {
     return (
       <div>
-        Posts New
+        <div className="text-xs-right">
+          <Link className="btn btn-primary" to="/">
+            Home
+          </Link>
+        </div>
+        <form>
+          <Field
+            name="title"
+            component={this.renderTitleField}
+          />
+        </form>
       </div>
     );
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostsNew);
